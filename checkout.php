b2a04900e0928149e3547e6e48bd035b0053f0ab
Cole Holyoake <coleholyoake@live.com>
2016-02-01 13:28:23 +1300

    added cart with table

---
+++ checkout.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+//include header template
+include "templates/header.template.php";
+
+//display contents
+include "templates/cart-contents.template.php";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+include "templates/footer.template.php";
